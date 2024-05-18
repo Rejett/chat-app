dbc2e21c7cb6c0e0e21bd66f46d1e31a5d36e7c8
Matheus Carmo de Araújo <rerrol2508@gmail.com>
2024-05-18 16:17:55 -0300

    Criação do endpoint de busca de usuários

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+import usersRoutes from "./routes/users.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
@@ -15,6 +16,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", usersRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
