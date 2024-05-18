c2e66547a8cbdb26895b84e9fa3d37e0ebe331f4
Matheus Carmo de Araújo <rerrol2508@gmail.com>
2024-05-18 17:02:58 -0300

    Criação do desing da tela de login

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,45 @@
+export default function Login() {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto ">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-blur-lg bg-opacity-0">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          Login
+          <span className="text-blue-500"> ChatApp</span>
+        </h1>
+        <form>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text text-gray-100">
+                UserName
+              </span>
+            </label>
+            <input
+              type="text"
+              placeholder="Digite o Usuário"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label">
+              <span className="text-base label-text text-gray-100">Senha</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Digite sua Senha"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <a
+            href="#"
+            className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
+          >
+            {"Não"} Possui uma conta?
+          </a>
+          <div>
+            <button className="btn btn-block btn-sm mt-2">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
