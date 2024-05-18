6964614d014d9da1451552b3a7347eba74582546
Matheus Carmo de Araújo <rerrol2508@gmail.com>
2024-05-18 15:23:46 -0300

    criando a rota para envio de mensagem

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -79,7 +79,7 @@ export const login = async (req, res) => {
   }
 };
 
-export const logout = async (req, res) => {
+export const logout = async (res) => {
   try {
     res.clearCookie("jwt", "", { maxAge: 0 });
     res.status(200).json({ message: "Logged out successfully" });
