b1e56cc1ee892a470d819ce3043710b4e4549699
Matheus Carmo de Araújo <rerrol2508@gmail.com>
2024-05-19 16:56:28 -0300

    criação da funcionalidade de logout do site

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -79,12 +79,12 @@ export const login = async (req, res) => {
   }
 };
 
-export const logout = async (res) => {
+export const logout = (req, res) => {
   try {
     res.clearCookie("jwt", "", { maxAge: 0 });
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
-    console.log("Error in login controller : " + error.message);
-    res.status(500).json({ error: "Internal Server Eerror" });
+    console.log("Error in logout controller: " + error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
