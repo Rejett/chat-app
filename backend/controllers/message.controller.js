1e36b18a29bf93235c1defb2a54926105ca6b83d
Matheus Carmo de Araújo <rerrol2508@gmail.com>
2024-05-20 10:27:04 -0300

    deploy

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
   try {
@@ -27,9 +28,15 @@ export const sendMessage = async (req, res) => {
       conversation.messages.push(newMessage._id);
     }
 
-    // SOCKET IO FUNCTIONALITY WILL GO HERE
-
     await Promise.all([conversation.save(), newMessage.save()]);
+
+    console.log("oi");
+    // SOCKET IO FUNCTIONALITY WILL GO HERE
+    const receiverSocketId = getReceiverSocketId(receiverId);
+
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
     res.status(201).send(newMessage);
     return; // Assegure-se de retornar para evitar execução adicional
--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,12 @@ import usersRoutes from "./routes/users.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import protectRoute from "./middleWare/protectRoute.js";
+import path from "path";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -19,7 +22,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", protectRoute, messageRoutes);
 app.use("/api/users", protectRoute, usersRoutes);
 
-app.listen(PORT, () => {
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server listening on port ${PORT}`);
 });
